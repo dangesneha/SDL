b8e3caa1d472672e70ebc2876500c25aa4ff8590
dangesneha <snehadange1998@gmail.com>
2018-06-28 11:34:51 +0530

    string to list

---
+++ ass2.py
@@ -0,0 +1,15 @@
+def token(string):
+    start = 0
+    i = 0
+    token_list = []
+    for x in range(0, len(string)):
+        if " " == string[i:i+1]:
+            token_list.append(string[start:i+1])
+            #print string[start:i+1]
+            start = i + 1
+        i += 1
+    token_list.append(string[start:i+1])
+    return token_list
+
+print (token('I am sneha'))
+        
