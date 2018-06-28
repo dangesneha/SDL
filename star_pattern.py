1a246a9aab93b3339fa3e5498c16f62de8759271
dangesneha <snehadange1998@gmail.com>
2018-06-28 11:26:40 +0530

    pattern of star

---
+++ star_pattern.py
@@ -0,0 +1,18 @@
+x=8
+n=0
+for i in range(8):
+ for k in range(x):
+  print(' ',end=" ")
+ x=x-1
+ for j in range(i):
+  print('*',end=" ")
+ for m in range(n):
+  print('*',end=" ")
+  n=n+9
+ print()
+
+
+    
+     
+   
+
